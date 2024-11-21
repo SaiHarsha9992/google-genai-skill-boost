245c885d5821a93e9078be8177fab0f4167493b1
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2024-11-21 22:01:08 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+//main page
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
